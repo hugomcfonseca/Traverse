b0c240995a4f10f95f301c2cd0a24fb5a99ae83e
Filipe Morais <Filipe_morais@outlook.pt>
2015-12-27 19:48:09 +0000

    Revert "mais alterações finais"
    This reverts commit 4b79c6021d4a30054a2975b87c8c8c7bdc8fd9be.

--- app/src/main/java/feup/traverse/Session.java
+++ app/src/main/java/feup/traverse/Session.java
@@ -1,6 +1,5 @@
 package feup.traverse;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
@@ -20,29 +19,8 @@ public class Session {
         prefs.edit().putString("username", username).apply();
     }
 
-    public void putdata(String username, String name, String email, String date,  String password) {
-        prefs.edit().putString("username", username).apply();
-        prefs.edit().putString("name", name).apply();
-        prefs.edit().putString("email", email).apply();
-        prefs.edit().putString("date", date).apply();
-        prefs.edit().putString("password", password).apply();
-
-    }
-
     public String getusername() {
-        return prefs.getString("username","");
+        String username = prefs.getString("username","");
+        return username;
     }
-    public String getname() {
-        return prefs.getString("name","");
-    }
-    public String getdate() {
-        return prefs.getString("date","");
-    }
-    public String getemail() {
-        return prefs.getString("email","");
-    }
-    public String getpassword() {
-        return prefs.getString("password","");
-    }
-
 }
